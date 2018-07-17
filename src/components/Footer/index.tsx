e3d9043ff8149d73e95960099344577593ce1dcc
Trev <jfanor@outlook.com>
2018-07-17 01:25:20 -0400

    Added data might not be 100% accurate in the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ class Footer extends React.Component {
     render() {
         return <footer className="footer max-width">
             <p>Created by <a href="https://blizztrack.com/">Blizztrack</a></p>
+            <p>Data might not be 100% accurate</p>
             <a href="http://discord.gg/ac2HKXx">Join our discord here</a> <a href="https://www.patreon.com/BlizzTrack">Help us out on Patreon</a>
         </footer>;
     }
